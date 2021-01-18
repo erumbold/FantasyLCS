fc6345f6791ccdb6740c70c70fe9dca1ee5d8209
Erika Rumbold <erumbold@ithaca.edu>
2021-01-18 17:17:31 -0600

    created Trade static template

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def roster():
 def matchups():
     return render_template("matchups.html")
 
+@app.route('/trade')
+def trade():
+    return render_template("trade.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
