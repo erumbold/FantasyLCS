50da89cea4702a7037c03501699ff05b82f754a6
Erika Rumbold <erumbold@ithaca.edu>
2021-01-19 20:15:29 -0600

    created log in static template

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ def matchups():
 def trade():
     return render_template("trade.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
